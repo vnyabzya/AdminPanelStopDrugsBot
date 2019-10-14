97473d311463d7052344e8cb5eceb6bdf608048b
Остап <ostap@MacBook-Air.local>
2019-10-15 00:33:50 +0300

    Новая обнова

--- app.py
+++ app.py
@@ -304,7 +304,9 @@ def edit_telegram_shop(telegram_shop_id):
         if 'url' in session:
             return redirect(session['url'])
         else:
-            return redirect(url_for('index'))
+            return redirect(url_for('edit_telegram_shop',
+                                    telegram_shop_id=Shop.query.filter(Shop.checked_by_admin == False,
+                                                                       Shop.telegram_shop != None).first()))
     else:
         form.telegram_link.data = telegram_shop_obj.telegram_link
         form.regions.data = [region.id for region in telegram_shop_obj.regions]
